c05575ad4124311ad41ee93296977cf1fb127ec3
Dmitri Smirnov <dmitri.snirnov@mail.com>
2013-07-16 17:08:44 +0200

    initial commit for colors game

---
+++ www/js/main.js
@@ -0,0 +1,119 @@
+(function(){
+	// 
+	var dimensions,
+		colors = [
+			{
+				name: 'Красный',
+				code: 'E41B17'
+			},{
+				name: 'Синий',
+				code: '1589FF'
+			},{
+				name: 'Зеленый',
+				code: '4CC417'
+			},{
+				name: 'Желтый',
+				code: 'FFFF00'
+			},{
+				name: 'Оранжевый',
+				code: 'FFA500'
+			},{
+				name: 'Белый',
+				code: 'ffffff'
+			},{
+				name: 'Черный',
+				code: '000000'
+			},{
+				name: 'Коричневый',
+				code: 'A03200'
+			},{
+				name: 'Фиолетовый',
+				code: '7D0552'
+			},{
+				name: 'Серый',
+				code: '808080'
+			}
+		],
+		tpl_button = '',
+		gameSize = 12,
+		hiddenCount = 0;
+
+	var startGame = function(){
+		var aTemp = colors.slice(0),
+			len = aTemp.length, newLen = len,
+			i = 0, rnd, data = [],
+			count = 0, currentTile,
+			bgClass = 'bg-0' + Math.floor(Math.random() * 6);
+			
+		document.body.className = bgClass;
+		
+		for(; i <= len/2; i++){
+			rnd = Math.floor(Math.random() * newLen);
+			data.push(aTemp[rnd]);
+			aTemp.splice(rnd,1);
+			newLen--;
+		}
+		
+		data = data.concat(data).shuffle();
+		//console.log(data);
+		
+		var game = document.getElementById('game'),
+			template = '<li><div class="panel click ds-pair-tile color-{{code}}" data-id="{{code}}"><div class="front"></div><div class="back" style="background-color:#{{code}}"></div></div></li>';
+
+		game.innerHTML = tpl(template, data);
+ 
+
+		var handler = function(e){
+			e.preventDefault && e.preventDefault();
+			e.stopImmediatePropagation && e.stopImmediatePropagation();
+			var el = $(this),
+				id = this.getAttribute('data-id');
+			
+			
+			if(!el.hasClass('flip')){
+				if(count == 2){
+					$('.panel').removeClass('flip');
+					count = 0;
+					currentTile = '';
+				}
+				if(count == 1 && currentTile == id){
+					hiddenCount++;
+					setTimeout(function(){
+						$('.color-' + id).addClass('hide');
+						
+						if(hiddenCount == 6) {
+							$('.buttonPush').css({display: 'block'});
+							hiddenCount = 0;
+						}
+					}, 300);
+				}
+				currentTile = id;
+				el.addClass('flip');
+				count++;
+			}
+		};
+
+		$('.click').on('touchstart', handler);
+		$('.click').on('click', handler);
+
+	};
+	
+	var init = function(){
+		//dimensions = getDimensions();
+	};
+	
+	init();
+	
+	var start = function(){
+		//console.log(dimensions);
+		//addMarkers();
+		$('.buttonPush').css({display: 'none'});
+		startGame();
+	};
+	
+	
+	window.DS = {
+		start: start
+	};
+}());
+
---
+++ www/js/utils.js
@@ -0,0 +1,79 @@
+;(function () {
+	// Accepts a template and data. Searches through the
+	// data, and replaces each key in the template, accordingly.
+	var attachTemplateToData = function(template, data) {
+		var i = 0,
+			len = data.length,
+			fragment = '';
+
+		// For each item in the object, make the necessary replacement
+		function replace(obj) {
+			var t, key, reg;
+ 
+			for (key in obj) {
+				reg = new RegExp('{{' + key + '}}', 'ig');
+				t = (t || template).replace(reg, obj[key]);
+			}
+			return t;
+		}
+		for (; i < len; i++) {
+			fragment += replace(data[i]);
+		}
+		return fragment;
+	};
+
+	this.tpl = attachTemplateToData;
+	
+	
+	
+	
+	var addMarkers = function(){
+		var markersHtml = '<i class="ds-m1"></i><i class="ds-m2"></i><i class="ds-m3"></i><i class="ds-m4"></i>';
+		var el = document.createElement("div");
+		el.className = 'ds-markers';
+		el.innerHTML = markersHtml;
+		document.body.appendChild(el);
+	};
+	
+	var getDimensions = function(){
+		return { 
+			viewportWidth: document.documentElement.clientWidth,
+			viewportHeight: document.documentElement.clientHeight,
+			deviceWidth: window.screen.width,
+			deviceHeight: window.screen.height,
+			orientation: window.orientation
+		}
+	};
+	
+	var orientationChage = function(e){
+		dimensions = getDimensions();
+		console.log(dimensions);
+	};
+
+	
+
+	
+	
+})();
+
+
+
+/* Array.shuffle( deep ) - перемешать элементы массива случайным образом
+
+deep - необязательный аргумент логического типа, указывающий на то, 
+       нужно ли рекурсивно обрабатывать вложенные массивы;
+       по умолчанию false (не обрабатывать)
+*/
+Array.prototype.shuffle = function( b )
+{
+ var i = this.length, j, t;
+ while( i ) 
+ {
+  j = Math.floor( ( i-- ) * Math.random() );
+  t = b && typeof this[i].shuffle!=='undefined' ? this[i].shuffle() : this[i];
+  this[i] = this[j];
+  this[j] = t;
+ }
+
+ return this;
+};
