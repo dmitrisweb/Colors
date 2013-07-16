e2b6b4c7e72852aef9c6cea29014478da5334c10
Dmitri Smirnov <dmitry@BB-SYSTEM-0144.backbase.com>
2013-07-16 17:04:43 +0200

    Version 0.1

--- www/js/index.js
+++ www/js/index.js
@@ -18,7 +18,8 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function(callback) {
+		this.callback = callback;
         this.bindEvents();
     },
     // Bind Event Listeners
@@ -37,13 +38,14 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+		if(this.callback)this.callback();
+        // var parentElement = document.getElementById(id);
+        // var listeningElement = parentElement.querySelector('.listening');
+        // var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        // listeningElement.setAttribute('style', 'display:none;');
+        // receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        // console.log('Received Event: ' + id);
     }
 };
